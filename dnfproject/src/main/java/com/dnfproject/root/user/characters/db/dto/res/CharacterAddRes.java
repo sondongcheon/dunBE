d2aabb8cafa0b3d54537c69b9ca0903a5dd0dc01
손동천 <kij8025@naver.com>
2026-02-08 00:00:55 +0900

    a

--- dnfproject/src/main/java/com/dnfproject/root/user/characters/db/dto/res/CharacterAddRes.java
+++ dnfproject/src/main/java/com/dnfproject/root/user/characters/db/dto/res/CharacterAddRes.java
@@ -1,46 +1,24 @@
 package com.dnfproject.root.user.characters.db.dto.res;
 
-import com.dnfproject.root.user.characters.db.entity.CharactersEntity;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CharacterAddRes {
-    private Long id;
-    private Long adventureId;
-    private String charactersId;
-    private String server;
-    private String charactersName;
-    private String jobGrowName;
-    private Integer fame;
-    private String memo;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime createAt;
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime updateAt;
 
-    public static CharacterAddRes from(CharactersEntity character) {
-        return CharacterAddRes.builder()
-                .id(character.getId())
-                .adventureId(character.getAdventure().getId())
-                .charactersId(character.getCharactersId())
-                .server(character.getServer())
-                .charactersName(character.getCharactersName())
-                .jobGrowName(character.getJobGrowName())
-                .fame(character.getFame())
-                .memo(character.getMemo())
-                .createAt(character.getCreateAt())
-                .updateAt(character.getUpdateAt())
-                .build();
+    private List<FailedItem> failed;
+
+    @Getter
+    @AllArgsConstructor
+    public static class FailedItem {
+        private String characterName;
+        private String reason;
     }
 }
--- dnfproject/src/main/java/com/dnfproject/root/user/characters/service/CharacterService.java
+++ dnfproject/src/main/java/com/dnfproject/root/user/characters/service/CharacterService.java
@@ -1,11 +1,12 @@
 package com.dnfproject.root.user.characters.service;
 
+import com.dnfproject.root.user.characters.db.dto.req.AddCharactersReq;
 import com.dnfproject.root.user.characters.db.dto.req.UpdateCharacterMemoReq;
 import com.dnfproject.root.user.characters.db.dto.req.UpdateClearStateReq;
 import com.dnfproject.root.user.characters.db.dto.res.CharacterAddRes;
 
 public interface CharacterService {
-    CharacterAddRes addCharacter(String server, String characterName);
+    CharacterAddRes addCharacters(AddCharactersReq request);
 
     void updateMemo(UpdateCharacterMemoReq request, Long adventureId);
 
